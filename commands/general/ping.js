0d250eb46686eb7b60fbdd2c87a82af8605652d7
parasop <63485824+parasop@users.noreply.github.com>
2021-04-25 18:20:37 +0530

    Create ping.js

---
+++ commands/general/ping.js
@@ -0,0 +1,12 @@
+module.exports = {
+  info: {
+    name: "ping",
+    aliases: "",
+    description: `Show bots Latency`,
+},
+
+  run: async function (client, message, args) {
+
+message.channel.send("pong")
+
+  }}
